17d376d0cd1be0600de8a7e017c4ea6c15e6bef3
ueihvn <letronghieu3007@gmail.com>
2021-06-27 11:28:42 +0700

    add teacher.go

--- data/teacher.go
+++ data/teacher.go
@@ -1,18 +1,68 @@
 package data
 
 type Teacher struct {
-	Id        int
-	Mail      string
-	FullName  string
-	UserName  string
-	Password  string
-	SubjectId int
+	Id        int    `db: "id"`
+	Mail      string `db:"mail"`
+	FullName  string `db:"full_name"`
+	UserName  string `db:"user_name"`
+	Password  string `db:"password"`
+	SubjectId int    `db:"subject_id"`
 }
 
 func (teacher *Teacher) CreateTeacher() (err error) {
-	_, err = Db.NamedExec(`insert into teachers (mail, full_name,  user_name, password, subject_id) values (:mail, :full_name, :user_name, :password, :subject_id)`, teacher)
+	err = Db.QueryRow("insert into teachers (mail, full_name, user_name, password, subject_id) values ($1, $2, $3, $4, $5) returning id",
+		teacher.Mail,
+		teacher.FullName,
+		teacher.UserName,
+		teacher.Password,
+		teacher.SubjectId).Scan(&teacher.Id)
 	if err != nil {
 		return
 	}
+	return
+}
+
+func GetTeachersBySubjectId(subjectId int) (teachers []Teacher, err error) {
+	rows, err := Db.Queryx("select id, mail, full_name, user_name, password, subject_id from teachers where subject_id = $1", subjectId)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		teacher := Teacher{}
+		err = rows.StructScan(teacher)
+		if err != nil {
+			return
+		}
+		teachers = append(teachers, teacher)
+	}
+	rows.Close()
+	return
+}
+
+func GetAllTeachers() (teachers []Teacher, err error) {
+	rows, err := Db.Queryx("select id, mail, full_name, user_name, password, subject_id from teachers")
+	if err != nil {
+		return
+	}
+
+	for rows.Next() {
+		teacher := Teacher{}
+		err = rows.StructScan(teacher)
+		if err != nil {
+			return
+		}
+		teachers = append(teachers, teacher)
+	}
+	rows.Close()
+	return
+}
+
+func (teacher *Teacher) Update() (err error) {
+	_, err = Db.NamedExec(`update teachers set mail =:mail, full_name =:full_name, user_name =:user_name, password =:password,subject_id =:subject_id where id =:id`, teacher)
+	return
+}
+
+func (teacher *Teacher) Delete() (err error) {
+	_, err = Db.Exec("delete from students where id = $1", teacher.Id)
 	return
 }
